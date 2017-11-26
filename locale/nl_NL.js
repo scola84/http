f8be10fe57656ee786fdf0d579a0f532e5765828
M.C. van den Boogaart <scola@kvdb.name>
2017-11-27 00:30:19 +0100

    Add strings

--- locale/nl_NL.js
+++ locale/nl_NL.js
@@ -1,12 +1,18 @@
 export default {
   error: {
     long: {
+      400: 'De gegevens zijn niet juist.',
+      404: 'Het pad is niet gevonden.',
       500: 'Er is een serverfout opgetreden.',
-      502: 'Er is een verbindingsfout opgetreden.'
+      502: 'Er is een verbindingsfout opgetreden.',
+      504: 'Er is een verbindingsfout opgetreden.'
     },
     short: {
+      400: 'Gegevensfout',
+      404: 'Niet gevonden',
       500: 'Serverfout',
-      502: 'Verbindingsfout'
+      502: 'Verbindingsfout',
+      504: 'Verbindingsfout',
     }
   }
 };
