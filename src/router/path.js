50451ed341c41fccb95957de34fcba8feb528635
M.C. van den Boogaart <scola@kvdb.name>
2018-04-14 12:57:40 +0200

    Extract paths only once

--- src/router/path.js
+++ src/router/path.js
@@ -1,17 +1,28 @@
 import { Router } from '@scola/worker';
 
 export default class PathRouter extends Router {
+  constructor(options) {
+    super(options);
+    this._names = null;
+  }
+
   act(request, data, callback) {
+    if (this._names === null) {
+      this._names = Object.keys(this._workers);
+    }
+
     const url = request.parseUrl();
-    const paths = Object.keys(this._workers);
+
+    let name = null;
     let params = null;
 
-    for (let i = 0; i < paths.length; i += 1) {
-      params = new RegExp(paths[i]).exec(url.path);
+    for (let i = 0; i < this._names.length; i += 1) {
+      name = this._names[i];
+      params = new RegExp(name).exec(url.path);
 
       if (params !== null) {
         request.params = params;
-        this.pass(paths[i], request, data, callback);
+        this.pass(name, request, data, callback);
         return;
       }
     }
