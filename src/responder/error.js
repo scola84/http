33b7e2f91c254d00d829db0d6814030299c26bb0
M.C. van den Boogaart <scola@kvdb.name>
2018-05-12 15:30:58 +0200

    Remove error details from response

--- src/responder/error.js
+++ src/responder/error.js
@@ -1,15 +1,16 @@
+/*eslint no-useless-escape: 0 */
+
 import { Worker } from '@scola/worker';
 
 export default class ErrorResponder extends Worker {
   err(message, error, callback) {
     const response = message.createResponse();
-    const match = error.message.match(/^(\d{3})/);
+    const match = error.message.match(/(\d{3})?([^\(]*)/);
 
     let data = '';
 
-    response.status = match === null ? 500 : Number(match[1]);
-    data = response.status === 500 ? '' : error.message
-      .slice(match === 0 ? 0 : 4);
+    response.status = match === null ? 500 : Number(match[1] || 500);
+    data = response.status === 500 ? '' : match[2].trim();
 
     response.state.body = true;
 
