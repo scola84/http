0eb4e8386e09f56bb8a96ff414bb4935dd7c845b
M.C. van den Boogaart <scola@kvdb.name>
2019-01-30 22:11:36 +0100

    Refactor response transformer

--- src/transformer/response.js
+++ src/transformer/response.js
@@ -2,23 +2,19 @@ import { Worker } from '@scola/worker';
 
 export default class ResponseTransformer extends Worker {
   act(response, data, callback) {
-    const [box, extraData] = this._resolveResponse(response);
-
-    if (response.status >= 400) {
-      this._handleError(response, data, callback, box, extraData);
-      return;
+    try {
+      if (response.status >= 400) {
+        this._throwError(response, data);
+      } else {
+        this._handleSuccess(response, data, callback);
+      }
+    } catch (error) {
+      this._handleError(response, data, callback, error);
     }
-
-    this._handleSuccess(response, data, callback, box, extraData);
   }
 
   err(response, error, callback) {
-    const [box, extraData] = this._resolveResponse(response);
-
-    box.error = true;
-    error.data = extraData;
-
-    this.fail(box, error, callback);
+    this._handleError(response, null, callback, error);
   }
 
   merge(box, extraData, data, response) {
@@ -29,46 +25,50 @@ export default class ResponseTransformer extends Worker {
     return data;
   }
 
-  _handleError(response, data, callback, box, extraData) {
-    const description = data &&
-      (data.error && data.error.message || data.message) ||
-      '';
-
-    const message = `${response.status} ${description}`;
-    const error = new Error(message.trim());
-
-    box.error = true;
+  _handleError(response, data, callback, error) {
+    const {
+      box,
+      data: extraData,
+      callback: extraCallback = () => {}
+    } = response.request.extra;
 
     error.data = extraData;
     error.responseData = data;
+
+    extraCallback();
+
+    this.fail(box, error, callback);
+  }
+
+  _handleSuccess(response, data, callback) {
+    const {
+      box,
+      data: extraData,
+      callback: extraCallback = () => {}
+    } = response.request.extra;
+
+    data = this.merge(box, extraData, data, response);
+
+    extraCallback();
+
+    this.pass(box, data, callback);
+  }
+
+  _throwError(response, data) {
+    let description = '';
+
+    if (data && data.error && data.error.message) {
+      description = data.error.message;
+    }
+
+    const message = `${response.status} ${description}`;
+    const error = new Error(message.trim());
 
     if (data && data.error) {
       error.field = data.error.field;
       error.reason = data.error.reason;
     }
 
-    this.fail(box, error, callback);
-  }
-
-  _handleSuccess(response, data, callback, box, extraData) {
-    try {
-      data = this.merge(box, extraData, data, response);
-      this.pass(box, data, callback);
-    } catch (error) {
-      error.data = extraData;
-      error.responseData = data;
-      this.fail(box, error, callback);
-    }
-  }
-
-  _resolveResponse(response) {
-    const extra = response.request.extra;
-
-    if (extra.callback) {
-      extra.callback();
-      extra.callback = null;
-    }
-
-    return [extra.box, extra.data];
+    throw error;
   }
 }
