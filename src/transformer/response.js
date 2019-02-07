5a85195219dbecb8e0d4947aa7b147e3e734fda2
M.C. van den Boogaart <scola@kvdb.name>
2019-02-07 21:30:14 +0100

    Catch callback errors in response transformer

--- src/transformer/response.js
+++ src/transformer/response.js
@@ -2,16 +2,11 @@ import { Worker } from '@scola/worker';
 
 export default class ResponseTransformer extends Worker {
   act(response, data, callback) {
-    try {
-      if (response.request.error === true) {
-        return;
-      } else if (response.status >= 400) {
-        this._throwError(response, data);
-      } else {
-        this._handleSuccess(response, data, callback);
-      }
-    } catch (error) {
-      this._handleError(response, data, callback, error);
+    if (response.status >= 400) {
+      this._handleError(response, data, callback,
+        this._createError(response, data));
+    } else {
+      this._handleSuccess(response, data, callback);
     }
   }
 
@@ -27,14 +22,30 @@ export default class ResponseTransformer extends Worker {
     return data;
   }
 
+  _createError(response, data) {
+    let description = '';
+
+    if (data && data.error && data.error.message) {
+      description = data.error.message;
+    }
+
+    const message = `${response.status} ${description}`;
+    const error = new Error(message.trim());
+
+    if (data && data.error) {
+      error.field = data.error.field;
+      error.reason = data.error.reason;
+    }
+
+    return error;
+  }
+
   _handleError(response, data, callback, error) {
     const {
       box,
       data: extraData,
       callback: extraCallback = () => {}
     } = response.request.extra;
-
-    response.request.error = true;
 
     error.data = extraData;
     error.responseData = data;
@@ -47,9 +58,13 @@ export default class ResponseTransformer extends Worker {
       }
     }
 
-    extraCallback();
-
-    this.fail(box, error, callback);
+    try {
+      extraCallback();
+      this.fail(box, error, callback);
+    } catch (tryError) {
+      tryError.data = extraData;
+      this.fail(box, tryError, callback);
+    }
   }
 
   _handleSuccess(response, data, callback) {
@@ -68,26 +83,12 @@ export default class ResponseTransformer extends Worker {
       }
     }
 
-    extraCallback();
-
-    this.pass(box, data, callback);
-  }
-
-  _throwError(response, data) {
-    let description = '';
-
-    if (data && data.error && data.error.message) {
-      description = data.error.message;
+    try {
+      extraCallback();
+      this.pass(box, data, callback);
+    } catch (tryError) {
+      tryError.data = data;
+      this.fail(box, tryError, callback);
     }
-
-    const message = `${response.status} ${description}`;
-    const error = new Error(message.trim());
-
-    if (data && data.error) {
-      error.field = data.error.field;
-      error.reason = data.error.reason;
-    }
-
-    throw error;
   }
 }
