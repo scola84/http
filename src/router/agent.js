7c4f7757bf3bbf09ada1035672f9515cee328829
M.C. van den Boogaart <scola@kvdb.name>
2019-03-22 14:30:36 +0100

    Make routers more efficient

--- src/router/agent.js
+++ src/router/agent.js
@@ -3,25 +3,30 @@ import { Router } from '@scola/worker';
 export default class AgentRouter extends Router {
   constructor(options) {
     super(options);
-    this._names = null;
+    this._agents = null;
   }
 
   act(request, data, callback) {
-    if (this._names === null) {
-      this._names = Object.keys(this._workers);
+    if (this._agents === null) {
+      this._agents = Object.keys(this._workers).map((name) => {
+        return {
+          name,
+          regexp: new RegExp(name)
+        };
+      });
     }
 
-    const agent = request.getHeader('User-Agent');
+    const userAgent = request.getHeader('User-Agent');
 
-    let name = null;
+    let agent = null;
     let params = null;
 
-    for (let i = 0; i < this._names.length; i += 1) {
-      name = this._names[i];
-      params = new RegExp(name).exec(agent);
+    for (let i = 0; i < this._agents.length; i += 1) {
+      agent = this._agents[i];
+      params = agent.regexp.exec(userAgent);
 
       if (params !== null) {
-        this.pass(name, request, data, callback);
+        this.pass(agent.name, request, data, callback);
         return;
       }
     }
--- src/router/path.js
+++ src/router/path.js
@@ -3,26 +3,31 @@ import { Router } from '@scola/worker';
 export default class PathRouter extends Router {
   constructor(options) {
     super(options);
-    this._names = null;
+    this._paths = null;
   }
 
   act(request, data, callback) {
-    if (this._names === null) {
-      this._names = Object.keys(this._workers);
+    if (this._paths === null) {
+      this._paths = Object.keys(this._workers).map((name) => {
+        return {
+          name,
+          regexp: new RegExp(name)
+        };
+      });
     }
 
     const url = request.parseUrl();
 
-    let name = null;
+    let path = null;
     let params = null;
 
-    for (let i = 0; i < this._names.length; i += 1) {
-      name = this._names[i];
-      params = new RegExp(name).exec(url.path);
+    for (let i = 0; i < this._paths.length; i += 1) {
+      path = this._paths[i];
+      params = path.regexp.exec(url.path);
 
       if (params !== null) {
         request.params = params;
-        this.pass(name, request, data, callback);
+        this.pass(path.name, request, data, callback);
         return;
       }
     }
