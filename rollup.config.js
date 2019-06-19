2f23a9865fd4b3ad3353010d4c457ba8ad2d8efe
M.C. van den Boogaart <scola@kvdb.name>
2019-06-19 14:39:44 +0200

    Update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -4,34 +4,45 @@ import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
 import resolve from 'rollup-plugin-node-resolve';
 
+const input = './index.js';
+
+const external = [
+  'bcrypt',
+  'busboy',
+  'fs',
+  'fs-extra',
+  'http',
+  'msgpack-lite',
+  'net',
+  'parse5',
+  'shortid',
+  'tls'
+];
+
+const plugins = [
+  resolve(),
+  commonjs(),
+  builtins(),
+  json(),
+  buble()
+];
+
 export default [{
-  input: './index.js',
-  external: [
-    'bcrypt',
-    'busboy',
-    'fs',
-    'fs-extra',
-    'http',
-    'msgpack-lite',
-    'net',
-    'parse5',
-    'shortid',
-    'tls'
-  ],
-  output: [{
-    file: 'dist/http.cjs.js',
-    format: 'cjs'
-  }, {
+  input,
+  external,
+  output: {
     extend: true,
     file: 'dist/http.umd.js',
     format: 'umd',
     name: 'scola.http'
-  }],
-  plugins: [
-    resolve(),
-    commonjs(),
-    builtins(),
-    json(),
-    buble()
-  ]
+  },
+  plugins
+}, {
+  input,
+  external,
+  output: {
+    file: 'dist/http.cjs.js',
+    format: 'cjs'
+  },
+  plugins
 }];
