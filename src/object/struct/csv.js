db8999ac8c83445a56b1e29d530e7a73ebdf51a8
M.C. van den Boogaart <scola@kvdb.name>
2019-06-20 13:55:13 +0200

    Refactor statics

--- src/object/struct/csv.js
+++ src/object/struct/csv.js
@@ -9,7 +9,12 @@ const lineEndings = {
 };
 
 export class CsvStruct {
-  static checkQuote(data, isValue, isEscaped, i) {
+  constructor(options, data) {
+    this._options = options;
+    this._data = data;
+  }
+
+  checkQuote(data, isValue, isEscaped, i) {
     if (isValue === true) {
       if (data[i + 1] === 34) {
         isEscaped = !isEscaped;
@@ -22,55 +27,6 @@ export class CsvStruct {
     }
 
     return [true, isValue, isEscaped, i];
-  }
-
-  static detectDelimiter(data, lineEndingCode) {
-    data = String(data.slice(0, data.indexOf(lineEndingCode)));
-
-    let count = 0;
-    let delimiter = ',';
-    let match = null;
-
-    for (let i = 0; i < delimiters.length; i += 1) {
-      match = data.match(new RegExp(delimiters[i]), 'g');
-      delimiter = match && match.length > count ? delimiters[i] : delimiter;
-      count = match && match.length > count ? match.length : count;
-    }
-
-    return delimiter;
-  }
-
-  static detectLineEnding(data) {
-    for (let i = 0; i < data.length; i += 1) {
-      if (data[i] === 13) {
-        if (data[i + 1] === 10) {
-          return 'CRLF';
-        }
-
-        return 'CR';
-      } else if (data[i] === 10) {
-        return 'LF';
-      }
-    }
-
-    return 'LF';
-  }
-
-  static formatValue(value, delimiter, regexp, quote) {
-    value = String(value);
-    return quote || value.match(regexp) ?
-      `"${value.replace(/"/g, '""')}"` : value;
-  }
-
-  static parseValue(data, begin, i, isQuoted) {
-    const quote = (isQuoted ? 1 : 0);
-    const value = data.slice(begin + quote, i - quote);
-    return String(value).replace(/""/g, '"');
-  }
-
-  constructor(options, data) {
-    this._options = options;
-    this._data = data;
   }
 
   decode() {
@@ -86,13 +42,13 @@ export class CsvStruct {
     const data = Buffer.from(this._data.trim());
 
     if (lineEnding === 'detect') {
-      lineEnding = CsvStruct.detectLineEnding(data);
+      lineEnding = this.detectLineEnding(data);
     }
 
     lineEnding = lineEndings[lineEnding];
 
     if (delimiter === 'detect') {
-      delimiter = CsvStruct.detectDelimiter(data, lineEnding.code);
+      delimiter = this.detectDelimiter(data, lineEnding.code);
     }
 
     delimiter = Buffer.from(delimiter)[0];
@@ -110,13 +66,13 @@ export class CsvStruct {
     for (; i < data.length; i += 1) {
       if (data[i] === 34) {
         ([isQuoted, isValue, isEscaped, i] =
-          CsvStruct.checkQuote(data, isValue, isEscaped, i));
+          this.checkQuote(data, isValue, isEscaped, i));
       } else if (data[i] === delimiter && isValue === false) {
-        line[line.length] = CsvStruct.parseValue(data, begin, i, isQuoted);
+        line[line.length] = this.parseValue(data, begin, i, isQuoted);
         isQuoted = false;
         begin = i + 1;
       } else if (data[i] === lineEnding.code && isValue === false) {
-        line[line.length] = CsvStruct.parseValue(data, begin, i, isQuoted);
+        line[line.length] = this.parseValue(data, begin, i, isQuoted);
         lines[lines.length] = line;
         line = [];
         isQuoted = false;
@@ -124,11 +80,43 @@ export class CsvStruct {
       }
     }
 
-    line[line.length] = CsvStruct.parseValue(data, begin, i, isQuoted);
+    line[line.length] = this.parseValue(data, begin, i, isQuoted);
     lines[lines.length] = line;
 
     this._data = lines;
     return this._data;
+  }
+
+  detectDelimiter(data, lineEndingCode) {
+    data = String(data.slice(0, data.indexOf(lineEndingCode)));
+
+    let count = 0;
+    let delimiter = ',';
+    let match = null;
+
+    for (let i = 0; i < delimiters.length; i += 1) {
+      match = data.match(new RegExp(delimiters[i]), 'g');
+      delimiter = match && match.length > count ? delimiters[i] : delimiter;
+      count = match && match.length > count ? match.length : count;
+    }
+
+    return delimiter;
+  }
+
+  detectLineEnding(data) {
+    for (let i = 0; i < data.length; i += 1) {
+      if (data[i] === 13) {
+        if (data[i + 1] === 10) {
+          return 'CRLF';
+        }
+
+        return 'CR';
+      } else if (data[i] === 10) {
+        return 'LF';
+      }
+    }
+
+    return 'LF';
   }
 
   encode() {
@@ -158,7 +146,7 @@ export class CsvStruct {
 
       csv += i > 0 ? delimiter : '';
 
-      csv += CsvStruct.formatValue(
+      csv += this.formatValue(
         field.label,
         delimiter,
         regexp,
@@ -175,7 +163,7 @@ export class CsvStruct {
         csv += j > 0 ?
           delimiter : '';
 
-        csv += CsvStruct.formatValue(
+        csv += this.formatValue(
           field.value(this._data[i], j),
           delimiter,
           regexp,
@@ -186,6 +174,18 @@ export class CsvStruct {
 
     this._data = csv;
     return this._data;
+  }
+
+  formatValue(value, delimiter, regexp, quote) {
+    value = String(value);
+    return quote || value.match(regexp) ?
+      `"${value.replace(/"/g, '""')}"` : value;
+  }
+
+  parseValue(data, begin, i, isQuoted) {
+    const quote = (isQuoted ? 1 : 0);
+    const value = data.slice(begin + quote, i - quote);
+    return String(value).replace(/""/g, '"');
   }
 
   toString() {
