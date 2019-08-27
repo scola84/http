9959f6fc61b62ec08ba9caafd3545962f5fd610f
M.C. van den Boogaart <scola@kvdb.name>
2019-08-27 13:19:16 +0200

    Prepare publish

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,10 @@
-import babel from 'rollup-plugin-babel'
-import builtins from 'rollup-plugin-node-builtins'
-import commonjs from 'rollup-plugin-commonjs'
-import json from 'rollup-plugin-json'
-import resolve from 'rollup-plugin-node-resolve'
+import { rollup } from '@scola/worker'
+import { name, version } from './package.json'
+
+const {
+  banner,
+  plugins
+} = rollup
 
 const external = [
   '@scola/worker',
@@ -32,27 +34,11 @@ const globals = {
 
 const input = './index.js'
 
-const plugins = [
-  resolve(),
-  commonjs(),
-  builtins(),
-  json(),
-  babel({
-    plugins: [
-      ['@babel/plugin-transform-runtime', {
-        helpers: false
-      }]
-    ],
-    presets: [
-      ['@babel/preset-env']
-    ]
-  })
-]
-
 export default [{
   input,
   external,
   output: {
+    banner: banner(name, version),
     extend: true,
     file: 'dist/http.umd.js',
     format: 'umd',
@@ -64,6 +50,7 @@ export default [{
   input,
   external,
   output: {
+    banner: banner(name, version),
     file: 'dist/http.cjs.js',
     format: 'cjs',
     globals
